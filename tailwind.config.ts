d9d241983412d5933d2ba8cfee77fa955bf280ad
Youjin-cmd <y.kimiau@gmail.com>
2024-01-02 13:36:52 +0900

    ⚙️ Chore. config and lint settings

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,25 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss"
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    extend: {},
+    colors: {
+      white: "#FFFFFF",
+      mattBlack: "#393939",
+      black: "#000000",
+      lightGrey: "#F5F5F5",
+      grey: "#D9D9D9",
+      darkGrey: "#B1B1B1",
+      yellow: "#F4F0CD",
+      red: "#D33434",
+      redHover: "#DB7474",
+      blue: "#4285F4",
+      blueHover: "#679df5",
     },
   },
   plugins: [],
