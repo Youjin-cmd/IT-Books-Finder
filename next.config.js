965a4b8b8b14445afe4a0cd72b45082439bfbf9a
Youjin-cmd <y.kimiau@gmail.com>
2024-01-02 22:46:51 +0900

    ⚙️ Chore. adjust eslint extentions

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+/** @type {import("next").NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "itbook.store",
+        port: "",
+        pathname: "/img/**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
