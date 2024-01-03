b5e9a43d545c6e7cb00589991d886ab97560a2e0
Youjin-cmd <y.kimiau@gmail.com>
2024-01-04 00:40:24 +0900

    💥 Feat. add IBookDetail type as global

--- src/types/type.ts
+++ src/types/type.ts
@@ -6,3 +6,18 @@ export interface IBook {
   title: string;
   url: string;
 }
+
+export interface IBookDetail {
+  title: string;
+  subtitle: string;
+  authors: string;
+  publisher: string;
+  isbn13: string;
+  pages: string;
+  year: string;
+  rating: string;
+  desc: string;
+  price: string;
+  image: string;
+  url: string;
+}
