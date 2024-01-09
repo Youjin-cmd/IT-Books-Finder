1889dd7658c84ae266af80acc4984f4ae18991b8
Youjin-cmd <y.kimiau@gmail.com>
2024-01-09 16:39:45 +0900

    🧹Refactor. remove rewrites from  next config

--- next.config.js
+++ next.config.js
@@ -11,22 +11,6 @@ const nextConfig = {
       },
     ],
   },
-  async rewrites() {
-    return [
-      {
-        source: "/new",
-        destination: "https://api.itbook.store/1.0/new",
-      },
-      {
-        source: "/search/:path*",
-        destination: "https://api.itbook.store/1.0/search/:path*",
-      },
-      {
-        source: "/books/:path*",
-        destination: "https://api.itbook.store/1.0/books/:path*",
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
